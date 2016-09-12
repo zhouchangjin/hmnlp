fdd7d7d66ed84ce1b64ef1a3995dab1898116b7f
changjin <changjin@DESKTOP-CKU8V9O>
2016-09-12 17:54:29 +0800

    implement standart normal distribution CDF( cumulative distribution function)

--- src/main/java/com/harmonywisdom/math/probability/distribution/NormalDistribution.java
+++ src/main/java/com/harmonywisdom/math/probability/distribution/NormalDistribution.java
@@ -11,10 +11,24 @@ public class NormalDistribution {
 		return result;
 	}
 	
+	public static double StanddardCDFFunction(double value){
+		double sum=value;
+		double midValue=value;
+		
+		for(int i=1;i<=100;i++){
+			midValue=midValue*value*value/(2*i+1);
+			sum=sum+midValue;
+		}
+		double result=0.5+(sum/Math.sqrt(2*Math.PI))*Math.pow(Math.E, -1.0*value*value/2);
+		return result;
+		
+	}
+	
 
 	
 	public static void main(String args[]){
 		System.out.println(probability(0,1,2));
+		System.out.println(StanddardCDFFunction(1));
 	}
 
 }
